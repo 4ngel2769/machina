15a1838b6c3d6048e74555d8d272ac4757cf9c74
4ngel2769 <angeldevz0@proton.me>
2025-10-30 19:16:46 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { ConditionalLayout } from "./conditional-layout";
-import { Toaster } from 'sonner';
+import { Toaster } from '@/components/ui/toaster';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
